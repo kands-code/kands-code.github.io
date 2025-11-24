103612c5341a267c76e1208ba135c31c2a812d43
kands-code <qarks@icloud.com>
2025-11-24 12:08:11 +0800

    update: use katex in mdbook

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-941efb81.js',
+                path_to_root + 'searchindex-ec233cf3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
