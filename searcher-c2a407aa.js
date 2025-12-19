efd966f9912b0a8f9923e4ef02d848b5bc9214b7
kands-code <qarks@icloud.com>
2025-12-19 17:51:03 +0800

    update: theme for ksl

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-18860cf8.js',
+                path_to_root + 'searchindex-0c9c2568.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
