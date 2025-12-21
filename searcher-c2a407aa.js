fb47bfffdb7076b858c9df76edf88748de0ab396
kands-code <qarks@icloud.com>
2025-12-21 17:12:12 +0800

    typo: sicp-ex02

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0c9c2568.js',
+                path_to_root + 'searchindex-2f192341.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
