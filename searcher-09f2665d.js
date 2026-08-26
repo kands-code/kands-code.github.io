46e2cad78432a96f931bc4ef9ace1cf2f798ec08
kands-code <qarks@icloud.com>
2026-08-26 23:43:41 +0800

    update: use shade on gnome

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-197b1e62.js',
+                path_to_root + 'searchindex-f460ce5f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
